328914a6d7530716e18c7629a5c7b88699e2112e
Ramazan <coldalbard.@gmail.com>
2023-01-31 21:06:42 +0300

    +

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
-﻿// int size1 = 3;
-// int size2 = 3;
+﻿// int size1 = 4;
+// int size2 = 4;
 
+// // Функция для заполнения двумерного массива
 // string[,] CreateXOMassiv(int size1, int size2)
 // {
 //     string[,] xo_massiv = new string[size1, size2];
@@ -9,12 +10,14 @@
 //         for (int j = 0; j < size2; j++)
 //         {
 //             xo_massiv[i, j] = ".";
+//             xo_massiv[i, 0] = $"{i}";
+//             xo_massiv[0, j] = $"{j}";
 //         }
 //     }
 //     return xo_massiv;
 // }
-// // Функция для заполнения двумерного массива
 
+// // Функция для вывода массива на экран
 // void Print3dArray(string[,] xo_massiv)
 // {
 //     for (int i = 0; i < xo_massiv.GetLength(0); i++)
@@ -27,104 +30,119 @@
 //     }
 //     Console.WriteLine();
 // }
-// // Функция для вывода массива на экран
 
-// void PlayersMoves(string[,] xo_massiv)
+// // Функция для выполнения ходов игроками
+// string[,] TakeInput(string[,] xo_massiv)
 // {
 //     int index = 1;
-//     string Player1 = String.Empty;
-//     string Player2 = String.Empty;
-//     if (Player1 != "Victory - Player 1" || Player2 != "Victory - Player 2")
+//     for (int i = 0; i < index; i++)
 //     {
 //         index++;
-//         for (int k = 0; k <= index; k++)
+//         Console.Write("Enter the line you want to put [X] in: ");
+//         int x1 = Convert.ToInt32(Console.ReadLine());
+//         Console.Write("Enter the column you want to put [X] in: ");
+//         int x2 = Convert.ToInt32(Console.ReadLine());
+//         if (xo_massiv[x1, x2] == "x" || xo_massiv[x1, x2] == "o" || x1 > 3 || x1 < 1 || x2 > 3 || x2 < 1) TakeInput(xo_massiv);
+//         else
 //         {
-//             Console.Write("x1: ");
-//             int x1 = Convert.ToInt32(Console.ReadLine());
-//             Console.Write("x2: ");
-//             int x2 = Convert.ToInt32(Console.ReadLine());
 //             xo_massiv[x1, x2] = "x";
 //             Console.WriteLine();
 //             Print3dArray(xo_massiv);
-//             Console.Write("y1: ");
-//             int y1 = Convert.ToInt32(Console.ReadLine());
-//             Console.Write("y2: ");
-//             int y2 = Convert.ToInt32(Console.ReadLine());
-//             xo_massiv[y1, y2] = "o";
-//             for (int i = 0; i < xo_massiv.GetLength(0); i++)
-//             {
-//                 for (int j = 0; j < xo_massiv.GetLength(1); j++)
-//                 {
-//                     if (xo_massiv[0, 0] == "x"
-//                         && xo_massiv[0, 1] == "x"
-//                         && xo_massiv[0, 2] == "x"
-//                         || xo_massiv[1, 0] == "x"
-//                         && xo_massiv[1, 1] == "x"
-//                         && xo_massiv[1, 2] == "x"
-//                         || xo_massiv[2, 0] == "x"
-//                         && xo_massiv[2, 1] == "x"
-//                         && xo_massiv[2, 2] == "x"
-//                         || xo_massiv[0, 0] == "x"
-//                         && xo_massiv[1, 0] == "x"
-//                         && xo_massiv[2, 0] == "x"
-//                         || xo_massiv[0, 1] == "x"
-//                         && xo_massiv[1, 1] == "x"
-//                         && xo_massiv[2, 1] == "x"
-//                         || xo_massiv[0, 2] == "x"
-//                         && xo_massiv[1, 2] == "x"
-//                         && xo_massiv[2, 2] == "x"
-//                         || xo_massiv[0, 0] == "x"
-//                         && xo_massiv[1, 1] == "x"
-//                         && xo_massiv[2, 2] == "x"
-//                         || xo_massiv[0, 2] == "x"
-//                         && xo_massiv[1, 1] == "x"
-//                         && xo_massiv[2, 0] == "x")
-//                     {
-//                         Player1 = "Victory - Player 1";
-//                         break;
-//                     }
-//                     if (xo_massiv[0, 0] == "o"
-//                         && xo_massiv[0, 1] == "o"
-//                         && xo_massiv[0, 2] == "o"
-//                         || xo_massiv[1, 0] == "o"
-//                         && xo_massiv[1, 1] == "o"
-//                         && xo_massiv[1, 2] == "o"
-//                         || xo_massiv[2, 0] == "o"
-//                         && xo_massiv[2, 1] == "o"
-//                         && xo_massiv[2, 2] == "o"
-//                         || xo_massiv[0, 0] == "o"
-//                         && xo_massiv[1, 0] == "o"
-//                         && xo_massiv[2, 0] == "o"
-//                         || xo_massiv[0, 1] == "o"
-//                         && xo_massiv[1, 1] == "o"
-//                         && xo_massiv[2, 1] == "o"
-//                         || xo_massiv[0, 2] == "o"
-//                         && xo_massiv[1, 2] == "o"
-//                         && xo_massiv[2, 2] == "o"
-//                         || xo_massiv[0, 0] == "o"
-//                         && xo_massiv[1, 1] == "o"
-//                         && xo_massiv[2, 2] == "o"
-//                         || xo_massiv[0, 2] == "o"
-//                         && xo_massiv[1, 1] == "o"
-//                         && xo_massiv[2, 0] == "o")
-//                     {
-//                         Player2 = "Victory - Player 2";
-//                         break;
-//                     }
-//                 }
-//             }
 //             Console.WriteLine();
-//             Print3dArray(xo_massiv);
 //         }
+//         if (ChekWin(xo_massiv) == "Victory - Player 1" || ChekWin(xo_massiv) == "Victory - Player 2" || ChekWin(xo_massiv) == "Draw!") return xo_massiv;
+//         Console.Write("Enter the line you want to put [O] in: ");
+//         int y1 = Convert.ToInt32(Console.ReadLine());
+//         Console.Write("Enter the column you want to put [O] in: ");
+//         int y2 = Convert.ToInt32(Console.ReadLine());
+//         if(xo_massiv[y1, y2] == "x" || xo_massiv[y1, y2] == "o" || y1 > 3 || y1 < 1 || y2 > 3 || y2 < 1) TakeInput(xo_massiv);
+//         else
+//         {
+//         xo_massiv[y1, y2] = "o";
+//         Console.WriteLine();
+//         Print3dArray(xo_massiv);
+//         }
+//         if (ChekWin(xo_massiv) == "Victory - Player 1" || ChekWin(xo_massiv) == "Victory - Player 2" || ChekWin(xo_massiv) == "Draw!") return xo_massiv;
 //     }
-//     if (Player1 == "Victory - Player 1") Console.WriteLine(Player1);
-//     else if (Player2 == "Victory - Player 2") Console.WriteLine(Player2);
-//     else Console.WriteLine("Draw!");
+//     return xo_massiv;
 // }
 
+// // Функция для того чтобы узнать победителя
+// string ChekWin(string[,] xo_massiv)
+// {
+//     string Player = "-1";
+//     for (int i = 0; i < xo_massiv.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < xo_massiv.GetLength(1); j++)
+//         {
+//             if (xo_massiv[1, j] != "." && xo_massiv[2, j] != "."
+//             && xo_massiv[3, j] != "." && xo_massiv[i, 1] != "."
+//             && xo_massiv[i, 2] != "." && xo_massiv[i, 3] != "."
+//             && xo_massiv[1, 1] != "." && xo_massiv[2, 2] != "."
+//             && xo_massiv[3, 3] != "." && xo_massiv[1, 3] != "."
+//             && xo_massiv[2, 2] != "." && xo_massiv[3, 1] != ".") Player = "Draw!";
 
+//             else if (xo_massiv[1, 1] == "o"
+//             && xo_massiv[2, 2] == "o" && xo_massiv[3, 3] == "o") Player = "Victory - Player 2";
 
+//             else if (xo_massiv[1, 3] == "o"
+//             && xo_massiv[2, 2] == "o" && xo_massiv[3, 1] == "o") Player = "Victory - Player 2";
+
+//             else if (xo_massiv[1, 1] == "x"
+//             && xo_massiv[2, 2] == "x" && xo_massiv[3, 3] == "x") Player = "Victory - Player 1";
+
+//             else if (xo_massiv[1, 3] == "x"
+//             && xo_massiv[2, 2] == "x" && xo_massiv[3, 1] == "x") Player = "Victory - Player 1";
+
+//             else if (xo_massiv[1, 1] == "o"
+//                 && xo_massiv[1, 2] == "o"
+//                 && xo_massiv[1, 3] == "o"
+//                 || xo_massiv[2, 1] == "o"
+//                 && xo_massiv[2, 2] == "o"
+//                 && xo_massiv[2, 3] == "o"
+//                 || xo_massiv[3, 1] == "o"
+//                 && xo_massiv[3, 2] == "o"
+//                 && xo_massiv[3, 3] == "o"
+//                 || xo_massiv[1, 1] == "o"
+//                 && xo_massiv[2, 1] == "o"
+//                 && xo_massiv[3, 1] == "o"
+//                 || xo_massiv[1, 2] == "o"
+//                 && xo_massiv[2, 2] == "o"
+//                 && xo_massiv[3, 2] == "o"
+//                 || xo_massiv[1, 3] == "o"
+//                 && xo_massiv[2, 3] == "o"
+//                 && xo_massiv[3, 3] == "o")
+//             {
+//                 Player = "Victory - Player 2";
+//             }
+//             else if (xo_massiv[1, 1] == "x"
+//                 && xo_massiv[1, 2] == "x"
+//                 && xo_massiv[1, 3] == "x"
+//                 || xo_massiv[2, 1] == "x"
+//                 && xo_massiv[2, 2] == "x"
+//                 && xo_massiv[2, 3] == "x"
+//                 || xo_massiv[3, 1] == "x"
+//                 && xo_massiv[3, 2] == "x"
+//                 && xo_massiv[3, 3] == "x"
+//                 || xo_massiv[1, 1] == "x"
+//                 && xo_massiv[2, 1] == "x"
+//                 && xo_massiv[3, 1] == "x"
+//                 || xo_massiv[1, 2] == "x"
+//                 && xo_massiv[2, 2] == "x"
+//                 && xo_massiv[3, 2] == "x"
+//                 || xo_massiv[1, 3] == "x"
+//                 && xo_massiv[2, 3] == "x"
+//                 && xo_massiv[3, 3] == "x")
+//             {
+//                 Player = "Victory - Player 1";
+//             }
+//         }
+//     }
+//     return Player;
+// }
 
 // string[,] xo_massiv = CreateXOMassiv(size1, size2);
 // Print3dArray(xo_massiv);
-// PlayersMoves(xo_massiv);
+// xo_massiv = TakeInput(xo_massiv);
+// string Player = ChekWin(xo_massiv);
+// Console.WriteLine(Player);
